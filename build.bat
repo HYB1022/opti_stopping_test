d39bb454007da353edffd7d8f6234e41ccffae9a
HYB1022 <banghyunwoo789@gmail.com>
2026-07-07 14:33:30 +0900

    빌드툴 추가

---
+++ build.bat
@@ -0,0 +1,23 @@
+﻿@echo off
+chcp 65001 > nul
+
+echo Cleaning...
+
+if exist build rmdir /s /q build
+if exist dist rmdir /s /q dist
+if exist __pycache__ rmdir /s /q __pycache__
+
+echo Building...
+
+pyinstaller ^
+--onefile ^
+--clean ^
+--collect-all matplotlib ^
+--icon=icon.ico ^
+--name OptimalStopping ^
+--upx-dir=upx ^
+main.py
+
+echo.
+echo Done!
+pause
